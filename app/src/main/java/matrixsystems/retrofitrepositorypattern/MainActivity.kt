9586434bf533f3f8527d5f79b42615dfb58edfe8
Shahbaz Hashmi <shahbaz@DEV-386.local>
2019-10-10 12:32:44 +0530

    added livedata callbacks

--- app/src/main/java/matrixsystems/retrofitrepositorypattern/MainActivity.kt
+++ app/src/main/java/matrixsystems/retrofitrepositorypattern/MainActivity.kt
@@ -2,11 +2,16 @@ package matrixsystems.retrofitrepositorypattern
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Observer
 import matrixsystems.retrofitrepositorypattern.network.APIService
 import matrixsystems.retrofitrepositorypattern.network.LoginRepository
+import matrixsystems.retrofitrepositorypattern.network.Resource
 import matrixsystems.retrofitrepositorypattern.network.ServiceGenerator
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
 
     lateinit var loginRepository: LoginRepository
 
@@ -16,7 +21,20 @@ class MainActivity : AppCompatActivity() {
 
         loginRepository = LoginRepository(ServiceGenerator.createService(APIService::class.java))
 
-        loginRepository.doLogin("9097769906", "123")
+        loginRepository.doLogin("9097769906", "123").observe(this, Observer {
+            when(it.status) {
+                Resource.Status.LOADING -> {
+                    Log.d(TAG, "api loading ...")
+                }
+                Resource.Status.SUCCESS -> {
+                    Log.d(TAG, "api success")
+                }
+                Resource.Status.ERROR -> {
+                    Log.d(TAG, "api error - ${it.apiError?.message}")
+                }
+            }
+        })
+
 
     }
 }
