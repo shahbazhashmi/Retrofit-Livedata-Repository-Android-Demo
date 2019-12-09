d46a486a0e3e13ec4fc82c634252b4e0fa24d405
Shahbaz Hashmi <shahbaz@DEV-386.local>
2019-12-09 23:48:22 +0530

    added logs on api test

--- app/src/main/java/matrixsystems/retrofitrepositorypattern/network/ErrorUtils.kt
+++ app/src/main/java/matrixsystems/retrofitrepositorypattern/network/ErrorUtils.kt
@@ -1,6 +1,7 @@
 package matrixsystems.retrofitrepositorypattern.network
 
 
+import android.text.TextUtils
 import okhttp3.ResponseBody
 import retrofit2.Converter
 import retrofit2.Response
@@ -12,19 +13,26 @@ import java.io.IOException
  */
 object ErrorUtils {
 
+    val ERROR_TEXT = "something went wrong !!!"
+
+    fun getError() : APIError = APIError(420, ERROR_TEXT)
+
     fun parseError(response: Response<*>): APIError {
         val converter : Converter<ResponseBody, APIError> = ServiceGenerator.retrofit
             .responseBodyConverter(APIError::class.java, arrayOfNulls<Annotation>(0))
 
-        var error: APIError?
+        var error: APIError? = null
 
         try {
-            error = converter.convert(response.errorBody())
-            if(error == null) {
+            if(response.errorBody() != null) {
+                error = converter.convert(response.errorBody()!!)
+            }
+            if(error == null || TextUtils.isEmpty(error.message)) {
                 throw IOException("null error body")
             }
-        } catch (e: IOException) {
-            error = APIError(404, e.localizedMessage)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            error = APIError(404, ERROR_TEXT)
         }
 
         return error!!
--- app/src/test/java/matrixsystems/retrofitrepositorypattern/ApiTest.kt
+++ app/src/test/java/matrixsystems/retrofitrepositorypattern/ApiTest.kt
@@ -1,12 +1,11 @@
 package matrixsystems.retrofitrepositorypattern
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.LiveData
-import junit.framework.Assert.*
 import matrixsystems.retrofitrepositorypattern.network.APIService
 import matrixsystems.retrofitrepositorypattern.network.Resource
 import matrixsystems.retrofitrepositorypattern.network.ServiceGenerator
 import matrixsystems.retrofitrepositorypattern.repositories.LoginRepository
+import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
 
@@ -23,8 +22,14 @@ class ApiTest {
     @Test
     fun doLoginRepository_positiveResponse() {
         val loginRepository  = LoginRepository(ServiceGenerator.createService(APIService::class.java))
-        val loginLiveData = loginRepository.doLogin("9876543210", "admin123")
-        assertEquals(loginLiveData.getOrAwaitValue().status, Resource.Status.SUCCESS)
+        val loginLiveData = loginRepository.doLogin("9876543210", "admin123").getOrAwaitValue()
+        if(loginLiveData.status == Resource.Status.ERROR) {
+            System.out.println("API ERROR -> ${loginLiveData.apiError}")
+        }
+        else {
+            System.out.println("API RESPONSE -> ${loginLiveData.data}")
+        }
+        assertEquals(loginLiveData.status, Resource.Status.SUCCESS)
     }
 
 
