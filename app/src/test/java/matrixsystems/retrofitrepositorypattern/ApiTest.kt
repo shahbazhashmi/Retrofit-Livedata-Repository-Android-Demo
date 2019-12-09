eb9c55f8e2994c5e568aa17ca7a9a3b5d42dc829
Shahbaz Hashmi <shahbaz@DEV-386.local>
2019-12-09 16:54:22 +0530

    added api test class

---
+++ app/src/test/java/matrixsystems/retrofitrepositorypattern/ApiTest.kt
@@ -0,0 +1,44 @@
+package matrixsystems.retrofitrepositorypattern
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.LiveData
+import matrixsystems.retrofitrepositorypattern.network.APIService
+import matrixsystems.retrofitrepositorypattern.network.Resource
+import matrixsystems.retrofitrepositorypattern.network.ServiceGenerator
+import matrixsystems.retrofitrepositorypattern.repositories.LoginRepository
+import org.junit.Rule
+import org.junit.Test
+
+/**
+ * Created by Shahbaz Hashmi on 2019-12-09.
+ */
+
+class ApiTest {
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+
+    @Test
+    fun fetchUserRepositories_positiveResponse() {
+        val loginRepository  = LoginRepository(ServiceGenerator.createService(APIService::class.java))
+
+        loginRepository.doLogin("9876543210", "admin123").observeForever {
+            when(it.status) {
+                Resource.Status.LOADING -> {
+                    //assert(false)
+                }
+                Resource.Status.SUCCESS -> {
+                    //assertEquals(Resource.Status.SUCCESS, it.status)
+                    assert(false)
+
+                }
+                Resource.Status.ERROR -> {
+                    assert(false)
+                }
+            }
+        }
+    }
+
+
+}
